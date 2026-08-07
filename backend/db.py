bfe4b6794b64c3ed1ce36fc4a24f54fe0b7e88a1
Julian David Mojica Lemus <juliandavid.pro99@gmail.com>
2026-08-06 20:29:28 -0500

    Actualizar db.py para usar DATABASE_URL en producción

--- backend/db.py
+++ backend/db.py
@@ -1,11 +1,24 @@
+import os
 import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def obtener_conexion():
-    conexion = psycopg2.connect(
+
+    database_url = os.getenv("DATABASE_URL")
+
+    # Producción (Neon / Render)
+    if database_url:
+
+        return psycopg2.connect(database_url)
+
+    # Desarrollo local
+    return psycopg2.connect(
+
         host="localhost",
         database="evolut",
         user="postgres",
         password="3134553"
-    )
 
-    return conexion
+    )
