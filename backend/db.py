cd8609b73528942031d9e1f04eeb2cda63ed2cd3
Julian David Mojica Lemus <juliandavid.pro99@gmail.com>
2026-08-20 22:28:57 -0500

    Configurar conexión para producción en Render

--- backend/db.py
+++ backend/db.py
@@ -1,11 +1,35 @@
+import os
 import psycopg2
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 
 def obtener_conexion():
 
+    database_url = os.getenv(
+        "DATABASE_URL"
+    )
+
+
+    # Producción
+
+    if database_url:
+
+        return psycopg2.connect(
+            dsn=database_url
+        )
+
+
+    # Desarrollo local
+
     return psycopg2.connect(
+
         host="localhost",
         database="evolut",
         user="postgres",
         password="3134553"
+
     )
--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,1 +1,1 @@
-const API_URL = "http://127.0.0.1:5000/api";
+const API_URL = "https://evolut-api-2qwc.onrender.com/api";
